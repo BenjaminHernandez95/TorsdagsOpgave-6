16eff7761943965a3e5b65ad066618f387330aa7
BenjaminHernandez95 <bennobarrales@gmail.com>
2023-10-12 14:08:34 +0200

    Færdig med TorsdagsOpgave-4

---
+++ Task4/src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle implements Shape {
+    double radius;
+    double længde;
+    public Circle(double radius,double længde){
+        this.radius=radius;
+        this.længde=længde;
+    }
+
+    /*public double getArea(double radius,double længde){
+        double area=Math.PI*(radius*radius)*længde;
+        return area;
+
+    }*/
+
+    public double getArea() {
+        double area=Math.PI*(radius*radius)*længde;
+        return area;
+    }
+}
---
+++ Task4/src/Interface.java
@@ -0,0 +1,13 @@
+interface Shape{
+    public double getArea();
+
+
+
+
+
+}
+
+
+
+
+
